c5f6b979e5f8d6b3897e589fae69a7ebf8c03067
David Fisher <tibbon@gmail.com>
2014-03-11 16:25:07 -0400

    Created basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D3Visualizer::Application.config.secret_key_base = '33d5e73984a1d0aed41ee7dadce2ed8348476fcc671bc7215a0f372182dd5023169b88d124184306a319a416719450d098ffba7f60e697dce21de9d76cba45be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+D3Visualizer::Application.config.session_store :cookie_store, key: '_d3_visualizer_session'
