a9a9c98cf4a217c522d94434100629333093d97b
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-03-14 17:10:04 -0400

    Fixed .gitignore, added missing 'end' to import rake task

--- lib/import/import_dataset.rb
+++ lib/import/import_dataset.rb
@@ -6,10 +6,12 @@ class ImportDataset
 
     data.each do |row|
       age = row[10]
+      gender = row[11]
 
       if age.present?
       user =  User.create!(username: row[1], playlists: row[4], play_count: row[5], subscriber: row[6], country: row[9], age: row[10], gender: row[11])
       puts "Added #{user.inspect}"
+      end
     end
 
   end
